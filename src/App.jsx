00500466e4e1d1c61c0aa72b2d03c27a905f7771
Zulfa094 <zulfa_94@hotmail.com>
2025-02-10 15:34:34 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,63 @@
-// src/App.jsx
+import './App.css'
+import WeatherForecast from './componenets/WeatherForecast/WeatherForcast'
 
 const App = () => {
-  return <h1>Hello world!</h1>
+  const weatherForecasts = [
+    {
+      day: 'Mon',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg',
+      imgAlt: 'sun icon',
+      conditions: 'sunny',
+      time: 'Morning'
+    },
+    {
+      day: 'Tue',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg',
+      imgAlt: 'moon icon',
+      conditions: 'clear',
+      time: 'Night'
+    },
+    {
+      day: 'Wed',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg',
+      imgAlt: 'clouds with lightning icon',
+      conditions: 'stormy',
+      time: 'All Day'
+    },
+    {
+      day: 'Thu',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg',
+      imgAlt: 'sun overcast by clouds icon',
+      conditions: 'overcast',
+      time: 'Evening'
+    },
+    {
+      day: 'Fri',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg',
+      imgAlt: 'moon overcast by clouds icon',
+      conditions: 'cloudy',
+      time: 'Night'
+    }
+  ]
+
+  return (
+    <>
+      <h1>Local Weather</h1>
+      <section>
+        {weatherForecasts.map((oneForecast) => (
+          <div key={oneForecast.day}>
+            <WeatherForecast
+              day={oneForecast.day}
+              img={oneForecast.img}
+              imgAl={oneForecast.imgAlt}
+              conditions={oneForecast.conditions}
+              time={oneForecast.time}
+            />
+          </div>
+        ))}
+      </section>
+    </>
+  )
 }
 
 export default App
---
+++ src/componenets/WeatherForecast/WeatherIcon.jsx
@@ -0,0 +1,10 @@
+const WeatherIcon = (props) => {
+  const { img, imgAlt } = props
+  return (
+    <div>
+      <img src={img} alt={imgAlt} />
+    </div>
+  )
+}
+
+export default WeatherIcon
